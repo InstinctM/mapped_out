57fafd7ca98c4b6ec0dcee22e71885ce05ad4be1
Ben Kollar <h45007bk@student.manchester.ac.uk>
2022-02-23 12:59:58 +0000

    commit changes

--- db/geocode.py
+++ db/geocode.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from argparse import ArgumentError
 from opencage.geocoder import OpenCageGeocode
 from opencage.geocoder import InvalidInputError, RateLimitExceededError, UnknownError
@@ -14,11 +15,26 @@ def add_post(link, description, author, latitude=None, longitude=None, location=
         pass #forward geocoding
 
     elif (location==None) and (longitude!=None and latitude!=None):
-        try:
-            results=geocoder.reverse_geocode(latitude,longitude,language='en')
-            if results and len(results):
-                print(results[0]['formatted'])
-        except RateLimitExceededError as err:
-            print(err)
-        except InvalidInputError as err:
-            print(err)
+        pass
+
+
+def forward_geocode(location):
+    try:
+        results=geocoder.forward_geocode(location,language='en',limit=1, annotations=1)
+        if results and len(results):
+            return (results[0]['geometry']['lat'],results[0]['geometry']['lng'])
+    except RateLimitExceededError as err:
+        print(err)
+    except InvalidInputError as err:
+        print(err)
+
+
+def reverse_geocode(latitude, longitude):
+    try:
+        results=geocoder.reverse_geocode(latitude,longitude,language='en',limit=1, annotations=1)
+        if results and len(results):
+            return (results[0]['formatted'])
+    except RateLimitExceededError as err:
+        print(err)
+    except InvalidInputError as err:
+        print(err)
