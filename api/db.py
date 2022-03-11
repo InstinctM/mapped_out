3e54340634bf0ae45e7d4daf66f27e251ab6cd63
Ben Kollar <benjamin.kollar@student.manchester.ac.uk>
2022-03-11 06:33:57 +0000

    add to db for use in import

--- api/db.py
+++ api/db.py
@@ -1,15 +1,22 @@
+#import this and use the functions to access the db
+
 from sqlalchemy import create_engine, Column, Integer, Float,String, null
 from sqlalchemy.orm import declarative_base, sessionmaker
 from opencage.geocoder import OpenCageGeocode, InvalidInputError, RateLimitExceededError, UnknownError
+from geopy import distance
 
 engine = create_engine('sqlite:///./mapped_out.db',echo=True,connect_args={"check_same_thread": False})
+
+opencageKey="06fee6e0f0fd4b82972c28992c487837"
+geocoder=OpenCageGeocode(opencageKey)
 
 Session=sessionmaker()
 Session.configure(bind=engine) #autocommit, autoflush params
 session=Session()
 
 Base=declarative_base()
-
+#use session.query (sqlalchemy) with record objects
+#represents records in post table
 class post(Base):
     __tablename__="posts"
 
@@ -17,57 +24,42 @@ class post(Base):
     link=Column(String,unique=True)
     description=Column(String)
     author=Column(String)
-    longitude=Column(Float)
     latitude=Column(Float)
+    longitude=Column(Float)
     location=Column(String)
 
     def __repr__(self):
         return '<post ( rowid=%s , link=%s , description=%s , author=%s , longitude=%s , latitude=%s , location=%s )> ' % (self.rowid,self.link,self.description,self.author,self.longitude,self.latitude,self.location)
 
+#records in user table
 class user(Base):
     __tablename__="users"
 
     rowid=Column(Integer, primary_key=True)
+    userid=Column(String, unique=True)
     name=Column(String)
     accessToken=Column(String, unique=True)
 
     def __repr__(self):
         return '<user ( rowid=%s , name=%s , accessToken=%s ) >' % ( self.rowid, self.name, self.accessToken)
 
-for post_entry in session.query(post):
-    print(repr(post_entry))
 
-for user_entry in session.query(user):
-    print(repr(user_entry))
+def add_post(p_link, p_desc, p_author, p_lat, p_long):
+    entry=post(link=p_link,author=p_author, description=p_desc, latitude=p_lat, longitude=p_long)
+    session.add(entry)
+    session.flush()
+
+def post_query_radius(latitude, longitude, radius): #assuming radius is in miles for now
+    matches=[]
+    for post_entry in session.query(post):
+        coord=(post_entry.latitude, post_entry.longitude)
+        if(distance.distance((latitude,longitude),coord).miles<radius):
+            matches.append(post_entry)
+    return matches
 
 
-
-opencageKey="06fee6e0f0fd4b82972c28992c487837"
-geocoder=OpenCageGeocode(opencageKey)
-
-def add_post(p_link, p_description, p_author, p_latitude=None, p_longitude=None, p_location=None):
-    #if given both coordinates and a text location (address) not going to check they are in the same place
-
-    assert ((p_longitude!=None and p_latitude!=None) or p_location!=None), "Incorrect location Data given"
-
-    if (p_longitude==None and p_latitude==None ) and p_location!=None:
-        result=forward_geocode(p_location)
-        if result!=null:
-            entry=post(link=p_link,description=p_description,latitude=result[0],longitude=result[1],location=p_location)
-            session.add(entry)
-            session.flush()
-        else:
-            print("unable to perform forward geocoding.") #TODO more informative error messages
-
-    elif (p_location==None) and (p_longitude!=None and p_latitude!=None):
-        result=reverse_geocode(p_latitude,p_longitude)
-        if result!=null:
-            entry=post(link=p_link,description=p_description,latitude=p_latitude,longitude=p_longitude,location=result)
-            session.add(entry)
-            session.flush()
-        else:
-            print("Unable to perform reverse geocoding.") #TODO ^
-
+#dont use these geocode functions (yet)
+'''
 def forward_geocode(location):
     try:
         results=geocoder.forward_geocode(location,language='en',limit=1, annotations=1)
@@ -92,3 +84,11 @@ def reverse_geocode(latitude, longitude):
     except InvalidInputError as err:
         print(err)
         return null
+
+def print_db():
+    for post_entry in session.query(post):
+        print(repr(post_entry))
+
+    for user_entry in session.query(user):
+        print(repr(user_entry))
+'''
