c0ab3b5de41f4863527446d8a9ee9af0b61fc307
Ben Kollar <benjamin.kollar@student.manchester.ac.uk>
2022-03-17 01:18:00 +0000

    commit changes for db table structure

--- api/db.py
+++ api/db.py
@@ -2,16 +2,12 @@
 
 from sqlalchemy import create_engine, Column, Integer, Float,String, null
 from sqlalchemy.orm import declarative_base, sessionmaker
-from opencage.geocoder import OpenCageGeocode, InvalidInputError, RateLimitExceededError, UnknownError
 from geopy import distance
 
 engine = create_engine('sqlite:///./mapped_out.db',echo=True,connect_args={"check_same_thread": False})
 
-opencageKey="06fee6e0f0fd4b82972c28992c487837"
-geocoder=OpenCageGeocode(opencageKey)
-
 Session=sessionmaker()
-Session.configure(bind=engine) #autocommit, autoflush params
+Session.configure(bind=engine)
 session=Session()
 
 Base=declarative_base()
@@ -21,9 +17,10 @@ class post(Base):
     __tablename__="posts"
 
     rowid=Column(Integer, primary_key=True) #this exists by default in sql tables
+    author=Column(Integer)
     link=Column(String,unique=True)
     description=Column(String)
-    author=Column(String)
+    likes=Column(Integer)
     latitude=Column(Float)
     longitude=Column(Float)
     location=Column(String)
@@ -36,16 +33,25 @@ class user(Base):
     __tablename__="users"
 
     rowid=Column(Integer, primary_key=True)
-    userid=Column(String, unique=True)
-    name=Column(String)
-    accessToken=Column(String, unique=True)
+    userid=Column(Integer, unique=True)
+    username=Column(String)
+    token=Column(String) 
+    tokenExpire=Column(Integer)
+    country=Column(String)
+    points=Column(Integer)
+
 
     def __repr__(self):
         return '<user ( rowid=%s , name=%s , accessToken=%s ) >' % ( self.rowid, self.name, self.accessToken)
 
 
-def add_post(p_link, p_desc, p_author, p_lat, p_long):
-    entry=post(link=p_link,author=p_author, description=p_desc, latitude=p_lat, longitude=p_long)
+def add_post(uid,lnk,desc,like_n,lat,long,loc):
+    entry=post(author=uid,link=lnk,description=desc,likes=like_n,latitude=lat,longitude=long,location=loc)
+    session.add(entry)
+    session.flush()
+
+def add_user(uid,uname,tkn,tokenexpr,cty,pnts):
+    entry=user(userid=uid,username=uname,token=tkn,tokenExpire=tokenexpr, country=cty, points=pnts)
     session.add(entry)
     session.flush()
 
@@ -56,39 +62,3 @@ def post_query_radius(latitude, longitude, radius): #assuming radius is in miles
         if(distance.distance((latitude,longitude),coord).miles<radius):
             matches.append(post_entry)
     return matches
-
-
-#dont use these geocode functions (yet)
-'''
-def forward_geocode(location):
-    try:
-        results=geocoder.forward_geocode(location,language='en',limit=1, annotations=1)
-        if results and len(results):
-            return (results[0]['geometry']['lat'],results[0]['geometry']['lng'])
-    except RateLimitExceededError as err:
-        print(err)
-        return null
-    except InvalidInputError as err:
-        print(err)
-        return null
-
-
-def reverse_geocode(latitude, longitude):
-    try:
-        results=geocoder.reverse_geocode(latitude,longitude,language='en',limit=1, annotations=1)
-        if results and len(results):
-            return (results[0]['formatted'])
-    except RateLimitExceededError as err:
-        print(err)
-        return null
-    except InvalidInputError as err:
-        print(err)
-        return null
-
-def print_db():
-    for post_entry in session.query(post):
-        print(repr(post_entry))
-
-    for user_entry in session.query(user):
-        print(repr(user_entry))
-'''
---
+++ api/init.py
@@ -0,0 +1,13 @@
+import db
+from sqlalchemy import MetaData
+metadata=MetaData()
+
+metadata.create_all(db.engine)
+
+db.add_user(69420,"General_Kenobi_XIV","verysnek",1668203967,"United Kingdom",999)
+db.add_user(123456,"John Smith","password",1669203964,"United States",1)
+db.add_user(98765,"Da Ali G", "password2",1669203964,"united Kingdom",13)
+
+db.add_post(69420,"https://www.youtube.com/watch?v=rEq1Z0bjdwc","Hello There.",11,53.46719182070933, -2.2341700730222045, "Kilburn Building")
+db.add_post(123456,"https://youtu.be/efgIm9YPZvE","Ali G inda house",22,53.464920527372755, -2.233294173022296, "Alan Gilbert Learning Commons")
+db.add_post(98765,"https://www.youtube.com/watch?v=2TvRITW4M3M","Bruh central",33,98765,1669203964,53.48358035907067, -2.241522635215619, "Manchester Arndale")
