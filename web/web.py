3187f61d8c75dbf7a4ee2051e3b07e88911e4787
Yat Long Poon <yatlong.poon@student.manchester.ac.uk>
2022-03-17 22:27:18 +0000

    Testing login requests

--- web/web.py
+++ web/web.py
@@ -2,15 +2,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__, static_folder='static')
 
+# Web Pages
 @app.route("/", methods = ["GET"])
 def landingPage():
     return render_template("map.html")
 
-
 @app.route("/about", methods = ["GET"])
 def aboutPage():
     return render_template("about.html")
-
 
 @app.route("/login", methods = ["GET"])
 def loginPage():
