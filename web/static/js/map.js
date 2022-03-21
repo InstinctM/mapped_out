f744e77f572dac0017424380464a6337a437e8c6
Yat Long Poon <yatlong.poon@student.manchester.ac.uk>
2022-03-21 18:41:10 +0000

    Get videos from backend and display markers

--- web/static/js/map.js
+++ web/static/js/map.js
@@ -1,14 +1,13 @@
+// use video link as key
+let videoMarkers = {};
+
+
 // Get user locatoin?
 let defaultLoc = [53.4674, -2.2339];  // [lat, lon]
-let defaultRad = 1500;  // radius is in meters
+let defaultRad = 15000;  // radius is in meters
 
 const map = L.map('map').setView(defaultLoc, 12);  // location, zoom
 
-
-const marker = L.marker(defaultLoc).addTo(map);
-const cicrle = L.circle(defaultLoc, {
-    radius: defaultRad, color: "red", fillColor: "green", fillOpacity: 0.2
-}).addTo(map);
 
 L.tileLayer(
     'https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=aEvyw1adgGE2cO8JBfZJ',
@@ -18,29 +17,80 @@ L.tileLayer(
 ).addTo(map);
 
 map.on('click', onMapClick);
+map.on('moveend', onMapMove);
+
+
+loadPosts();
+
+function addMarkers(newMarkers) {
+    videoMarkers.forEach((marker) => {
+        if (!marker in newMarkers) {
+            marker.remove()
+        }
+    });
+    videoMarkers = newMarkers;
+}
+
+function loadPosts(lat = defaultLoc[0], lon = defaultLoc[1], rad = defaultRad) {
+    httpGet(API_URL + "/get-posts", {
+        lat: lat,
+        lon: lon,
+        radius: rad,
+    }, (response) => {
+        //console.log(response);
+        // response is an array of post objects
+
+        response.forEach((post) => {
+            let lat = post["latitude"];
+            let lon = post["longitude"];
+            let link = post["link"];
+
+            if (videoMarkers[link] == null) {
+                let marker = L.marker([lat, lon]).addTo(map);
+                marker.bindPopup(getPopupElement(post), { maxWidth: "auto" });
+                videoMarkers[link] = marker;
+            }
+        });
+
+    });
+}
+
+
+function onMapMove(e) {
+    const RAD_SCALE = 69;  // Convert degrees to miles (nice)
+    let lat = map.getCenter()["lat"];
+    let lon = map.getCenter()["lng"];
+    let bounds = map.getBounds();
+    let dx = bounds.getEast() - bounds.getWest();
+    let dy = bounds.getNorth() - bounds.getSouth();
+    let rad = Math.max(dx, dy) * RAD_SCALE;
+    //console.log(`Map moved: [${lat}, ${lon}] => ${rad}`);
+
+    loadPosts(lat, lon, rad);
+}
 
 
 function onMapClick(e) {
     let lat = e.latlng.lat;
     let lon = e.latlng.lng;
-
-    //console.log(`Marker location: [${lat}, ${lon}]`);
-
-    marker.setLatLng([lat, lon]);
-    cicrle.setLatLng([lat, lon]);
-
-    marker.bindPopup(getPopupElement(lat, lon));
 }
 
+function getPopupElement(post) {
+    let link = post["link"];
 
-function getPopupElement(lat, lon) {
-    // Get videos in this location
     let popupElement = `
-    <h4>Video at [${lat.toFixed(4)}, ${lon.toFixed(4)}]</h4>
-    <div class="border border-dark" style="width: 240px; height: 144px;">
-    video preview
+    <div class="marker-popup" style="width: 480px; height: 400px;">
+        <h4>${post["description"]}</h4>
+        <div class="mx-auto" style="width: 460px; height: 260px;">
+        <iframe width="460" height="260" src="${link}"
+            allowfullscreen="allowfullscreen"
+        ></iframe>
+        </div>
+        <br>
+        <a class="mt-1" href="${link}">${link}</a>
+        <p class="my-1">by <b>${post["username"]}</b></p>
+        <p class="my-1">${post["likes"]} likes</p>
     </div>
-    <p>https://youtube.url.here</p>
     `;
 
     return popupElement;
