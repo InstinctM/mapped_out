b0980c848d3b1f6711783a599f4ce9838bdc7b4c
Yat Long Poon <yatlong.poon@student.manchester.ac.uk>
2022-03-30 18:33:44 +0100

    Cookies again found that google actually put session cookies and we use local storage anyways only popup for the first time and when scrolled down in landing page

--- web/static/js/script.js
+++ web/static/js/script.js
@@ -1,32 +1,46 @@
 var slideIndex = 1;
 showSlides(slideIndex);
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+    showSlides(slideIndex += n);
 }
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+    showSlides(slideIndex = n);
 }
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if(n > slides.length) {
-    slideIndex = 1
-  }
-  if(n < 1) {
-    slideIndex = slides.length
-  }
-  for(i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for(i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
 }
 
+function hideCookiePrompt() {
+    localStorage.setItem("cookie-accept", true);
+    document.getElementById("cookie-prompt").style.display = "none";
+}
 
-
-
-
+// Check cookie consent
+document.addEventListener("scroll", (e) => {
+    console.log(this.scrollY, document.documentElement.clientHeight);
+    // only show when scrolled down
+    if (this.scrollY > document.documentElement.clientHeight) {
+        let cookieAccept = localStorage.getItem("cookie-accept");
+        if (cookieAccept == null) {
+            document.getElementById("cookie-prompt").style.display = "block";
+        }
+    } else {
+        document.getElementById("cookie-prompt").style.display = "none";
+    }
+});
