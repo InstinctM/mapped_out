1c5383f5659eb3d9e405e09e74f939eba0a956e0
Ben Kollar <benjkollar03@gmail.com>
2022-04-01 14:52:09 +0100

    add more posts

--- api/init.py
+++ api/init.py
@@ -37,10 +37,23 @@ db.add_user(db.user(
     "HK",
     0,
 ))
+db.add_user(db.user(
+   -8278426939280095492,
+    "TheTraveller",
+    "a0f3285b07c26c0dcd2191447f391170d06035e8d57e31a048ba87074f3a9a15",
+    "92e75a672e05a198e67971b8aadd641fa372a11aca31a576bf2e56a108389419",
+    1648823445,
+    "AF",
+    0
+    ))
+
+
 
 db.add_post(db.post(31415,"https://www.youtube.com/embed/kkLUneUgiI8","Kilburn Building Tour",0,53.46719182070933, -2.2341700730222045, "Kilburn Building, Oxford Road, Manchester, M13 9PL, United Kingdom"))
 db.add_post(db.post(43837,"https://www.youtube.com/embed/pQN-pnXPaVg","HTML is a programming language",0,53.464920527372755, -2.233294173022296, "Alan Gilbert Learning Commons, Lime Grove, Manchester, M13 9PP, United Kingdom"))
 db.add_post(db.post(12345,"https://www.youtube.com/embed/2TvRITW4M3M","Bruh central",0,53.48358035907067, -2.241522635215619, "Arndale Centre, Halle Place, Manchester, M4 2HU, United Kingdom"))
-
+db.add_post(db.post(-8278426939280095492,"https://www.youtube.com/embed/huqJUghX26Y","Grand Canyon",0,37.4356124041315,-110.72296142578125,"Halls Crossing, San Juan County, Utah, United States of America"))
+db.add_post(db.post(-8278426939280095492,"https://www.youtube.com/embed/aZ-31-p3saU","Effiel Tower Guide",0,48.85596226389368,2.2979450225830083,"Field of Mars, Rue de l'Université, 75007 Ile-de-France, France"))
+db.add_post(db.post(-8278426939280095492,"https://www.youtube.com/embed/QA0WMIryzZs","Sydney Opera House",0,-33.8568428989929,151.21499955654147,"Sydney Opera House, 2 Macquarie Street, Sydney NSW 2000, Australia"))
 
 db.print_db()
